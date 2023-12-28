250517651dd3af88796151f772186efadcb2783e
imtiaz <imtiazahmed026@gmail.com>
2023-12-29 00:18:13 +0600

    moved array content to array file

--- src/array.ts
+++ src/array.ts
@@ -4,4 +4,4 @@ const arr2: tupleArray = [1, 2, 5];
 // arr2[2]= 'a';
 //! Type 'string' is not assignable to type 'number'.
 // arr2[12]= 1;
-//! Tuple type '[number, number, number]' of length '3' has no element at index '12'.
+// Tuple type '[number, number, number]' of length '3' has no element at index '12'.
